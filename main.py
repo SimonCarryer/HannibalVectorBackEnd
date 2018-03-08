133449d581dde4d9916978fc26df7032153d3823
DeanDM <dean.de.maret@gmail.com>
2018-03-09 11:58:50 +1300

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ app = Flask(__name__)
 def api_root():
     return jsonify(["Matt and Deans amazing adventures",
     "Why did Jenny fall asleep", "Pulp Fiction"])
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug = False)
