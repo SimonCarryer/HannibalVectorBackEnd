511706b980f5e20e892497fc21fa8608dab47309
Matt Bennett <matt.bennett@xero.com>
2018-03-09 16:49:34 +1300

    add movies

--- util/lookup.py
+++ util/lookup.py
@@ -11,6 +11,7 @@ class bidict(dict):
 
 class MovieGetter:
     def __init__(self, movie_to_matrix_dic, matrix):
+        movie_to_matrix_dic.index = range(len(movie_to_matrix_dic))
         self.movie_to_matrix_dic = bidict(movie_to_matrix_dic.IMDbId.to_dict())
         self.matrix = matrix
 
@@ -21,6 +22,6 @@ class MovieGetter:
     def get_closest(self, movie1, movie2, imdb_1, imdb_2):
         best_movie = movie1 + movie2
         print('Got some values %s %s' % (imdb_1, imdb_2))
-        best_list = [i for i in np.argsort(cosine_similarity(best_movie, self.matrix))[0][::-1] if self.movie_to_matrix_dic.inverse[i] not in [imdb_1, imdb_2]][:10]
+        best_list = [i for i in np.argsort(cosine_similarity(best_movie, self.matrix))[0][::-1] if self.movie_to_matrix_dic[i] not in [imdb_1, imdb_2]][:10]
 
-        return self.movie_to_matrix_dic[best_list]
+        return self.movie_to_matrix_dic[best_list[0]]
