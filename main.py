759562877b35ce85ee8c25632e5db4b884de2a38
DeanDM <dean.de.maret@gmail.com>
2018-03-09 16:19:08 +1300

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,17 @@ from flask import Flask, jsonify, request
 from util.process_pickle import load_compressed_pickled_object
 from util.lookup import MovieGetter
 
+print('starting app')
+
 print('gonna download the matrix')
 matrix = load_compressed_pickled_object("https://s3.amazonaws.com/hannibal-vector/hanibalVectorModel-09-03-2018.gz")
 print('got a matrix with shape %d, %d' % matrix.shape)
 
-app = Flask(__name__)
-
-
 print('gonna download the movie_to_matrix_dic')
-#movie_to_matrix_dic = load_compressed_pickled_object("TOBEREPLACED")
+movie_to_matrix_dic = load_compressed_pickled_object("https://s3.amazonaws.com/hannibal-vector/hanibalVectorIndex-09-03-2018.gz")
 print('got a movie_to_matrix_dic')
 
-#movie_getter = MovieGetter(movie_to_matrix_dic, matrix)
+movie_getter = MovieGetter(movie_to_matrix_dic, matrix)
 
 app = Flask(__name__)
 
@@ -25,11 +24,11 @@ def api_root():
 @app.route('/add')
 def addmovie():
     imdb_id_1 = request.args.get('movie_imdb_1')
-    #imdb_id_2 = request.args.get('movie_imdb_2')
-    #movie1 = movie_getter.get(imdb_id_1)
-    #movie2 = movie_getter.get(imdb_id_2)
-    #closest = movie_getter.get_closest(movie1, movie2, imdb_id_1, imdb_id_2)
-    return jsonify(imdb_id_1)
+    imdb_id_2 = request.args.get('movie_imdb_2')
+    movie1 = movie_getter.get(imdb_id_1)
+    movie2 = movie_getter.get(imdb_id_2)
+    closest = movie_getter.get_closest(movie1, movie2, imdb_id_1, imdb_id_2)
+    return jsonify(closest)
 
 @app.after_request
 def after_request(response):
